ddd415625ae9f9192607f3493b8371bf9e7a098b
emi1994 <60740212+emi1994@users.noreply.github.com>
2020-02-06 11:53:48 -0300

    Ejercicio 2 (primer clase)
    Uso de prompt

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,11 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+	nombre = prompt("Ingresa tu nombre");
+	console.log(nombre);
+
+	var nombre = prompt("Ingresa tu nombre");
+	console.log(nombre);
 }
 
