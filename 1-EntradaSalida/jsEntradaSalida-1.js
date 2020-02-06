9c7154708cb9ec75702b7ab4fa613b09ac7114d2
emi1994 <60740212+emi1994@users.noreply.github.com>
2020-02-06 11:12:48 -0300

    Ejercicio 1 (primer clase)
    Uso de alert
    Variables
    Valor Literal

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,23 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
-{
-	alert("ok");
+{	// Declaracion de VARIABLES
+	var primerPrecio;
+	var segundoPrecio;
+	var apellido = "nadie";
+
+	apellido = "Lenzuen";
+	alert(apellido);
+	// si esta entre comillas es un valor LITERAL
+	//alert("Tal cual");
+	//alert(apellido); // Variable
+	//alert("Apellido"); // Literal
+	//alert(); // Sale vacio
+
+	//alert(primerPrecio); //Undefined
+	//alert(preciopri); // No funciona
 }
+
+
+
+
 
