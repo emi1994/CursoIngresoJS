c365db0b5649273488d25e7c1a1acf06a589f757
emi1994 <60740212+emi1994@users.noreply.github.com>
2020-03-05 20:33:00 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,50 +1,130 @@
 function mostrar()
 {
-    var primerNumero;
-    var segundoNumero;
-    var sumaTotal;
+ var nombreCliente;
+ var edadCliente;
+ var sexoCliente;
+ var transaccion;
+ var importe;
+ var respuesta = "si";
+ var contador;
+ var contadorMujeres;
+ var contadorHombres;
+ var edadMinima;
+ var edadMaxima;
+ var sexoMaxima;
+ var nombreMinima;
+ var operacionMasUsada;
+ var acumulador;
 
-    //pedimos datos y definimos que son numeros enteros
-    primerNumero = prompt("Ingrese el primer numero");
-    primerNumero = parseInt(primerNumero);
+ contador = 0;
+ acumulador = 0;
+ contadorHombres = 0;
+ contadorMujeres = 0;
+ contadorDeposito = 0;
+ contadorExtraccion = 0;
+ edadMinima = 0;
+ sexoMaxima= 0;
 
-    segundoNumero = prompt("Ingrese el segundo numero");
-    segundoNumero = parseInt(segundoNumero);
-    
-    sumaTotal = primerNumero + segundoNumero;
-    sumaTotal = parseInt(sumaTotal);
-
-    //agregamos la logica y mostramos los resultados por alert
-    if(primerNumero == segundoNumero)
+    while(respuesta == "si")
     {
-        alert(primerNumero + " " + segundoNumero);
-    }else
-    {
-        if(primerNumero > segundoNumero)
+        nombreCliente = prompt("Ingrese el nombre del cliente");
+        while(!isNaN(nombreCliente))
         {
-            alert(primerNumero - segundoNumero);
+            nombreCliente = prompt("Ingrese un nombre valido");
+        }
+
+        edadCliente = prompt("Ingrese la edad del cliente (mayor a 18)");
+        edadCliente = parseInt(edadCliente);
+
+        while(isNaN(edadCliente) || edadCliente < 18)
+        {
+            edadCliente = prompt("Ingerese una edad valida");
+            edadCliente = parseInt(edadCliente);
+        }
+
+        sexoCliente = prompt("Ingrese el sexo del cliente f/m");
+        while(!isNaN(sexoCliente) || sexoCliente !="f" && sexoCliente !="m")
+        {
+            sexoCliente = prompt("Ingrese un sexo valido");
+        }
+
+        transaccion = prompt("Ingrese la transaccion: extraccion/deposito");
+        while(!isNaN(transaccion) || transaccion !="extraccion" && transaccion !="deposito")
+        {
+            transaccion = prompt("Ingrese una transaccion valida");
+            if(transaccion == "extraccion")
+            {
+                contadorExtraccion++;
+            }else
+                {
+                    contadorDeposito++;
+                }
+        }
+
+        importe = prompt("Ingrese el importe del cliente");
+        importe = parseInt(importe);
+
+        while(isNaN(importe) || importe < 0)
+        {
+            importe = prompt("Ingrese un importe valido");
+            importe  = parseInt(importe);
+        }
+
+        if(contador == 0 || edadCliente < edadMinima)
+        {
+            edadMinima = edadCliente;
+            nombreMinima = nombreCliente;
+        }
+
+        if(contador == 0 || edadCliente > edadMaxima)
+        {
+            edadMaxima = edadCliente;
+            sexoMaxima = sexoCliente;
+        }
+
+        if(sexoCliente == "m")
+        {
+            contadorHombres++;
         }else
+            {
+                contadorMujeres ++;
+            }
+        
+        if(contadorDeposito > contadorExtraccion)
         {
-            alert(primerNumero + segundoNumero);
-        }    
-        if(sumaTotal > 10)
-        {
-            alert("La suma es de " + sumaTotal + " y supero el 10");
-        } 
+            operacionMasUsada = "La operacion mas usada fue deposito";
+        }else
+            {
+                operacionMasUsada=  "La operacion mas usada fue extraccion";
+            }
+
+        contador ++;
+        acumulador = acumulador + edadCliente;
+        promedio = acumulador / contador;
+        respuesta = prompt("Desea continuar? si/no");
     }
-
-
 }
 
 
 
-/* Bienvenidos (IF). 
-Pedir dos números y mostrar el resultado: 
-Si son iguales los muestro concatenados. 
+/* necesito saber:
+el nombre del cliente
+la edad del cliente(debe ser mayor de edad)
+sexo
+tipo de transaccion(extraccion o deposito)
+importe (validar que no sea negativo)
 
+jose 37 m extraccion 1000
+maria 40 f deposito 2000
+jesus 33 m extraccion 1500
+melchor 20 m  extraccion 1300 
 
-Si el primero es mayor, los resto, 
-de lo contrario los sumo. 
+promedio de edad
+nombre del mas joven
 
-Si la suma es mayor a 10 ,además de mostrar el resultado, muestro el mensaje 
-"la suma es xxx y supero el 10". */
+cantidad de hombres=3
+cantidad de mujeres=1
+
+sexo del importe mas alto=f
+operacion mas utilizada=ext
+importe del mas viejo=2000 */
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,92 +1,160 @@
 function mostrar()
 {
-    var marca;
-    var pesoProducto;
-    var temperaturaAlmacenamiento;
-    var temperaturaPares;
-    var marcaMasPesado;
-    var pesoMaximo;
-    var productosEnCero;
-    var contador;
-    var contadorPares;
-    var respuesta;
-    var pesoPromedio;
-    var pesoMinimo;
-    contador = 0;
-    contadorPares = 0;
-    productosEnCero = 0;
-    respuesta = "si";
+   var marca;
+   var pesoProducto;
+   var temperaturaAlmacenamiento;
+   var respuesta;
+   var contador;
+   var contadorPares;
+   var temperaturasPares;
+   var contadorBajoCero;
+   var productoMaximo;
+   var marcaPesado;
+   var productoMinimo;
+   var promedio;
 
-    while(respuesta == "si")
-    {
-        marca = prompt("Ingrese el nombre de la marca del producto");
-        while(!isNaN(marca))
-        {
-            marca = prompt("Ingrese una marca valida");
-        }
+   productoMinimo =  0;
+   productoMaximo = 0;
+   respuesta = "si";
+   contador = 0;
+   contadorBajoCero = 0;
+   contadorPares = 0;
+   acumulador = 0;
 
-        pesoProducto = prompt("Ingrese el peso del producto");
-        pesoProducto = parseInt(pesoProducto);
-        while(pesoProducto < 0 || pesoProducto >100)
-        {
-            pesoProducto = prompt("Ingrese un peso entre 1~100");
-            pesoProducto = parseInt(pesoProducto);
-        }
+   while(respuesta == "si")
+   {
+       marca = prompt("Ingrese la marca de su producto");
+       while(!isNaN(marca))
+       {
+           marca = prompt("Ingrese solo letras");
+       }
+       pesoProducto = prompt("Ingrese el peso de su producto entre 1-100");
+       pesoProducto = parseInt(pesoProducto);
+       while(isNaN(pesoProducto) || pesoProducto < 1 || pesoProducto > 100)
+       {
+           pesoProducto = prompt("Ingrese un peso entre 1-100");
+           pesoProducto = parseInt(pesoProducto);
+       }
+       temperaturaAlmacenamiento = prompt("Ingrese la temperatura de almacenamiento entre -30 y 30");
+       temperaturaAlmacenamiento = parseInt(temperaturaAlmacenamiento);
 
-        temperaturaAlmacenamiento = prompt("Ingrese la temperatura de almacenamiento (entre -30 ~ 30 grados");
-        temperaturaAlmacenamiento = parseInt(temperaturaAlmacenamiento);
+       while(isNaN(temperaturaAlmacenamiento) || temperaturaAlmacenamiento < -30 || temperaturaAlmacenamiento > 30)
+       {
+           temperaturaAlmacenamiento = prompt("Ingrese una temperatura valida entre -30 y 30");
+           temperaturaAlmacenamiento = parseInt(temperaturaAlmacenamiento);
+       }
+       if(temperaturaAlmacenamiento%2 == 0)
+       {
+           contadorPares++;
+       }
 
-        while(temperaturaAlmacenamiento < -30 || temperaturaAlmacenamiento > 30)
-        {
-            temperaturaAlmacenamiento = prompt("Ingrese una temperatura valida");
-            temperaturaAlmacenamiento = parseInt(temperaturaAlmacenamiento);
-        }
+       if(contador == 0 || pesoProducto > productoMaximo)
+       {
+           productoMaximo = pesoProducto;
+           marcaPesado = marca;
+       }
 
-        if(temperaturaAlmacenamiento%2==0)
-        {
-            contadorPares ++;
-        }
+       if(contador == 0 || pesoProducto < productoMinimo)
+       {
+           productoMinimo = pesoProducto;
 
-        if(pesoProducto > pesoMaximo)
-        {
-            pesoMaximo = pesoProducto;
-            marcaMasPesado = pesoMaximo;
-        }
+       }
 
-        if(pesoProducto < pesoMinimo)
-        {
-            pesoMinimo = pesoProducto;
-        }
+       if(temperaturaAlmacenamiento < 0)
+       {
+           contadorBajoCero++;
+       }
 
-        if(temperaturaAlmacenamiento < 0)
-        {
-            productosEnCero ++;
-        }
-        contador ++;
-        pesoPromedio = pesoProducto / contador;
-        respuesta = prompt("Para continuar si/no");
-    }
+       contador ++;
+       acumulador = acumulador + pesoProducto;
+       promedio = acumulador / contador;
+       respuesta = prompt("Desea continuar? si/no");
+   }
 
-    console.log(temperaturaPares);
-    console.log(marcaMasPesado);
-    console.log(productosEnCero);
-    console.log(pesoPromedio);
-    console.log(pesoMaximo);
-    console.log(pesoMinimo);
+ 
 }
 
-
-
-
-
-
 /* Bienvenidos. 
-Realizar el algoritmo que permita ingresar la marca del producto, el peso el cual debe ser entre 1 y 100 (validar), 
-la temperatura de almacenamiento(entre -30 y 30) hasta que el usuario quiera e informar al terminar el ingreso por document.write: 
-a) La cantidad de temperaturas pares. 
-b) La marca del producto más pesado 
-c) La cantidad de productos que se conservan a menos de 0 grados. 
+Realizar el algoritmo que permita ingresar la marca del producto, el peso el cual debe ser entre 1 y 100 (validar),  ----
+la temperatura de almacenamiento(entre -30 y 30) hasta que el usuario quiera e informar al terminar el ingreso por document.write:  ----
+a) La cantidad de temperaturas pares. -------
+b) La marca del producto más pesado  ------
+c) La cantidad de productos que se conservan a menos de 0 grados.  -----
 
 
-d) El promedio del peso de todos los productos.	
+d) El promedio del peso de todos los productos.	-----
 f) El peso máximo y el mínimo. */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var marca;
+while(respuesta == "si")
+{
+    marca = prompt("Ingrese el nombre de la marca del producto");
+    while(!isNaN(marca))
+    {
+        marca = prompt("Ingrese una marca valida");
+    }
+
+    pesoProducto = prompt("Ingrese el peso del producto");
+    pesoProducto = parseInt(pesoProducto);
+    while(isNaN(pesoProducto) || pesoProducto < 0 || pesoProducto >100)
+    {
+        pesoProducto = prompt("Ingrese un peso entre 1~100");
+        pesoProducto = parseInt(pesoProducto);
+    }
+
+    temperaturaAlmacenamiento = prompt("Ingrese la temperatura de almacenamiento (entre -30 ~ 30 grados");
+    temperaturaAlmacenamiento = parseInt(temperaturaAlmacenamiento);
+
+    while(isNaN(temperaturaAlmacenamiento) || temperaturaAlmacenamiento < -30 || temperaturaAlmacenamiento > 30)
+    {
+        temperaturaAlmacenamiento = prompt("Ingrese una temperatura valida");
+        temperaturaAlmacenamiento = parseInt(temperaturaAlmacenamiento);
+    }
+
+    if(temperaturaAlmacenamiento%2==0)
+    {
+        contadorPares ++;
+    }
+
+    if(contador == 0 || pesoProducto > pesoMaximo)
+    {
+        pesoMaximo = pesoProducto;
+        marcaMasPesado = marca;
+    }
+
+    if(contador == 0 || pesoProducto < pesoMinimo)
+    {
+        pesoMinimo = pesoProducto;
+    }
+
+    if(temperaturaAlmacenamiento < 0)
+    {
+        productosBajoCero ++;
+    }
+    contador ++;
+    pesoPromedio = pesoProducto / contador;
+    respuesta = prompt("Para continuar si/no");
+}
+
+console.log(contadorPares);
+console.log(marcaMasPesado);
+console.log(productosBajoCero);
+console.log(pesoPromedio);
+console.log(pesoMaximo);
+console.log(pesoMinimo); */
